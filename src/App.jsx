83e022d7e412ee511965e41e4f8aaccce8ad826c
Jubril <jubfeb20@gmail.com>
2025-10-29 21:09:03 +0100

    Desktop Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import Header from "./Header";
 export default function App() {
   return (
     <>
-      <Header/>
+      <div>
+        <Header />
+        <hr className="hidden md:block m-auto border-0 h-px bg-[#E4E9F2] mt-[34px] md:w-[1110px]" />
+      </div>
     </>
   );
 }
+//[hsl(219, 35%, 92%)]
+// height: 2px;
+// color: grey;
+// background-color: green;
+// border-width: 0;
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,27 +2,39 @@ export default function Header() {
   return (
     <>
       <div>
-        <header className="flex">
-          {/* Logo bar */}
-          <div>
-            <img src="/images/logo.svg" />
-          </div>
+        <header className="flex md:w-[1110px] md:h-[50px] md:mt-7 items-center justify-between m-auto">
+          {/* Logo and list container */}
+          <div className="flex gap-[59px]">
+            {/* Logo bar */}
+            <div>
+              <img src="/images/logo.svg" />
+            </div>
 
-          {/* Collections */}
-          <div>
-            <ul>
-              <li>Collections</li>
-              <li>Men</li>
-              <li>Women</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
+            {/* Collections */}
+            <div className="hidden md:block">
+              <ul className="flex gap-[33px] text-[#69707D] font-semiboldl">
+                <li>Collections</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </div>
           </div>
 
           {/* Cart and Profile */}
-          <div></div>
+          <div className="flex gap-[46px] items-center">
+            <div>
+              <img src="images/icon-cart.svg" className="w-[22px] h-5" />
+            </div>
+            <div>
+              <img src="images/image-avatar.png" className="w-[50px]" />
+            </div>
+          </div>
         </header>
       </div>
     </>
   );
 }
+///images/icon-cart.svg
+///images/image-avatar.png
