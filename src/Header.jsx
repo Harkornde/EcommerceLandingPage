657ca7f30429e16d40e94385f13c9dffe87055b3
Jubril <jubfeb20@gmail.com>
2025-11-02 14:18:54 +0100

    Adding the toggle for mobile responsiveness

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [toggle, turnToglle] = useState(false);
+
   return (
     <>
       <div>
-        <header className="flex md:w-[1110px] md:h-[50px] mt-[19px] md:mt-7 items-center justify-around md:justify-between m-auto">
+        <header className="flex md:w-[1110px] md:h-[50px] mt-[19px] md:mt-7 items-baseline md:items-center justify-around md:justify-between m-auto">
+          <div
+            className={`fixed inset-0 bg-black transition-opacity duration-300 z-40 ${
+              toggle ? "opacity-75" : "opacity-0 pointer-events-none"
+            }`}
+            onClick={() => turnToglle(false)}
+          ></div>
+
           {/* Logo and list container */}
-          <div className="flex items-end md:items-center gap-4 md:gap-[59px]">
+          <div className="flex items-baseline gap-4 md:gap-[59px]">
             {/* Working with the modal */}
-            <div className="md:hidden" onClick={() => console.log("clicked")}>
-              <img src="images/icon-menu.svg" />
+            <div className="md:hidden">
+              <button
+                onClick={() => {
+                  turnToglle(!toggle);
+                  console.log(toggle);
+                }}
+              >
+                <img src="images/icon-menu.svg" />
+              </button>
             </div>
 
             {/* Logo bar */}
@@ -16,6 +34,30 @@ export default function Header() {
             </div>
 
             {/* Collections */}
+            <div
+              className={`absolute top-0 left-0 w-[250px] min-h-screen font-bold text-lg bg-[#FFFFFF] z-50  transition-transform duration-300 ease-in-out ${
+                toggle ? "translate-x-0" : "-translate-x-full"
+              } ${toggle ? "visible" : "invisible"} `}
+            >
+              <button
+                onClick={() => {
+                  turnToglle(!toggle);
+                }}
+              >
+                <img
+                  src="/images/icon-close.svg"
+                  className="w-3.5 h-3.5 mt-7 ml-[25px]"
+                />
+              </button>
+              <ul className="flex gap-5 mt-[54px] ml-[25px] flex-col text-[#1D2026] font-semibold">
+                <li>Collections</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+
             <div className="hidden md:block">
               <ul className="flex gap-[33px] text-[#69707D] font-semiboldl">
                 <li>Collections</li>
